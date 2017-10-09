6cf4518be0147e57e9bbe698a66bbf811ff8d6e4
0xfeedface1993 <318715498@qq.com>
2017-10-09 11:12:26 +0800

    Initial Commit

---
+++ EditorCore/EditorCore.h
@@ -0,0 +1,19 @@
+//
+//  EditorCore.h
+//  EditorCore
+//
+//  Created by virus1993 on 2017/10/9.
+//  Copyright © 2017年 ascp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EditorCore.
+FOUNDATION_EXPORT double EditorCoreVersionNumber;
+
+//! Project version string for EditorCore.
+FOUNDATION_EXPORT const unsigned char EditorCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EditorCore/PublicHeader.h>
+
+
